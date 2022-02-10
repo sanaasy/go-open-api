cb4b5de9802d7b952f4fd0c8bb9ad910fdb6cbde
Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>
2022-02-09 19:40:30 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/mvallati/go-cli
+
+go 1.16
+
+require (
+	github.com/PullRequestInc/go-gpt3 v1.1.3 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/sashabaranov/go-gpt3 v0.0.0-20201216121239-64c0048394ea // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// 	"strings"
+// )
+
+// func main() {
+// 	fmt.Println("Enter something: ")
+
+// 	reader := bufio.NewReader(os.Stdin)
+// 	text, _ := reader.ReadString('\n')
+// 	text = strings.Replace(text, "\n", "", -1)
+
+// 	fmt.Println(text)
+
+// 	os.Exit(0)
+// }
+
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/PullRequestInc/go-gpt3"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	apiKey := "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"
+	if apiKey == "" {
+		log.Fatalln("Missing API KEY")
+	}
+
+	text := getText()
+
+	ctx := context.Background()
+	client := gpt3.NewClient(apiKey)
+
+	// otherwise completion
+	resp, err := client.Completion(ctx, gpt3.CompletionRequest{
+		Prompt:    []string{text},
+		MaxTokens: gpt3.IntPtr(60),
+		Stop:      []string{"."},
+		Echo:      true,
+	})
+	// scope issue - move err and resp variables to outside if block
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(resp.Choices[0].Text + ".")
+}
+
+func getText() string {
+	fmt.Println("Enter something: ")
+
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", -1)
+
+	return text
+}
